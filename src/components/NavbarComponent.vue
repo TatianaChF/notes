1d7d83643581f40ab5e95c8b2124a35d6b302c03
TatianaChF <tatiana.chefox@gmail.com>
2025-03-04 19:47:09 +0300

    add content

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -1,3 +1,13 @@
 <template>
-
+  <nav>
+    <img src="../assets/images/logo.svg" alt="logo" />
+    <button>
+        <img src="../assets/images/login.svg" alt="login" />
+        Вход
+    </button>
+  </nav>
 </template>
+
+<script setup lang="ts">
+
+</script>
