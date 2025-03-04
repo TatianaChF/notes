fbd52635d12d6bf0eed34c657e035ee734008d01
TatianaChF <tatiana.chefox@gmail.com>
2025-03-04 21:14:34 +0300

    resize image

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,11 +1,14 @@
 <template>
 <main>
   <div class="main-content">
-    <div class="main-text">
+    <div>
       <h1>Мои заметки</h1>
       <h3>Не забывай о важном, храни его в облаке.</h3>
     </div>
-    <img src="../assets/images/main_image.svg" alt="logo" />
+    <img
+        src="../assets/images/main_image.svg"
+        alt="main image"
+        class="main-image" />
   </div>
 </main>
 </template>
@@ -18,20 +21,20 @@
   align-items: center;
 }
 
-.main-text {
-
-}
-
-.main-text h1 {
+h1 {
   color: #ffffff;
   font-size: 104px;
   font-weight: bold;
 }
 
-.main-text h3 {
+h3 {
   color: #9DA5AF;
   font-size: 32px;
   line-height: 48px;
   font-weight: bold;
+}
+
+.main-image {
+  width: 60%;
 }
 </style>
