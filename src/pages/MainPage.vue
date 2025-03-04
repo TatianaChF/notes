b2af92893cb291642cd5f80ec29ad47cafa0c137
TatianaChF <tatiana.chefox@gmail.com>
2025-03-04 20:58:52 +0300

    add main page content

---
+++ src/pages/MainPage.vue
@@ -0,0 +1,13 @@
+<template>
+<main>
+  <div>
+    <div>
+      <h1>Мои заметки</h1>
+      <p>Не забывай о важном, храни его в облаке.</p>
+    </div>
+    <img src="../assets/images/main_image.svg" alt="logo" />
+  </div>
+</main>
+</template>
+
+<script setup lang="ts"></script>
