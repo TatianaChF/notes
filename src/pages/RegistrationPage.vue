0f844f431a463928f0eb8e6829eb167f1b84eefa
TatianaChF <tatiana.chefox@gmail.com>
2025-03-05 20:33:19 +0300

    adding block content

--- src/pages/RegistrationPage.vue
+++ src/pages/RegistrationPage.vue
@@ -1,5 +1,23 @@
 <template>
-
+  <div>
+    <h2>Регистрация</h2>
+    <div>
+      <label for="email">Email</label>
+      <input name="email" type="email" />
+      <label for="password">Пароль</label>
+      <input name="password" type="password" />
+      <label for="passTwo">Пароль еще раз</label>
+      <input name="passTwo" type="password" />
+    </div>
+    <div>
+      <div>
+        <p>У вас есть аккаунт?</p>
+        <a href="#">Войдите</a>
+      </div>
+      <button>Зарегистрироваться</button>
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+</script>
