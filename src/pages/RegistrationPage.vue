a56e2a02e384bd40000354ae2f6d0a8edfc0c0aa
TatianaChF <tatiana.chefox@gmail.com>
2025-03-05 21:37:43 +0300

    adding styling

--- src/pages/RegistrationPage.vue
+++ src/pages/RegistrationPage.vue
@@ -1,23 +1,119 @@
 <template>
-  <div>
-    <h2>Регистрация</h2>
-    <div>
-      <label for="email">Email</label>
-      <input name="email" type="email" />
-      <label for="password">Пароль</label>
-      <input name="password" type="password" />
-      <label for="passTwo">Пароль еще раз</label>
-      <input name="passTwo" type="password" />
-    </div>
-    <div>
-      <div>
-        <p>У вас есть аккаунт?</p>
-        <a href="#">Войдите</a>
+  <div class="container-reg">
+    <div class="container">
+      <h2>Регистрация</h2>
+      <div class="input-container">
+        <div class="input-field">
+          <label for="email">
+            Email
+          </label>
+          <input
+              name="email"
+              type="email"
+              placeholder="Введите Email" />
+        </div>
+        <div class="input-field">
+          <label for="password">
+            Пароль
+          </label>
+          <input
+              name="password"
+              type="password"
+              placeholder="Введите пароль"
+          />
+        </div>
+        <div class="input-field">
+          <label for="passTwo">
+            Пароль еще раз
+          </label>
+          <input
+              name="passTwo"
+              type="password"
+              placeholder="Повторите пароль"
+          />
+        </div>
       </div>
-      <button>Зарегистрироваться</button>
+      <div class="link-container">
+        <div class="link">
+          <p>У вас есть аккаунт?</p>
+          <a href="#">Войдите</a>
+        </div>
+        <button>Зарегистрироваться</button>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 </script>
+
+<style scoped>
+.container-reg {
+  width: 780px;
+  height: 740px;
+  background-color: #1B2F46;
+  border-radius: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.input-container {
+  display: flex;
+  flex-direction: column;
+  margin-top: 30px;
+  gap: 20px;
+}
+
+.input-field {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+
+h2 {
+  color: #ffffff;
+  font-size: 48px;
+}
+
+label, p {
+  font-size: 18px;
+  color: #ffffff;
+}
+
+input {
+  width: 678px;
+  height: 72px;
+  border-radius: 36px;
+  border: none;
+  padding: 0 16px;
+}
+
+a {
+  font-size: 18px;
+  color: #B1C909;
+  font-weight: 700;
+  text-decoration: none;
+}
+
+button {
+  padding: 10px 24px;
+  background-color: #B1C909;
+  border-radius: 32px;
+  border: none;
+  font-size: 20px;
+  color: #fff;
+}
+
+.link-container {
+  display: flex;
+  justify-content: space-between;
+  margin-top: 30px;
+}
+
+.link {
+  display: flex;
+  align-items: center;
+  gap: 10px;
+}
+</style>
