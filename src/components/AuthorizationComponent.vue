b6af03621137a6aa922c53309cda1630da9697f2
TatianaChF <tatiana.chefox@gmail.com>
2025-03-11 17:49:46 +0300

    adding a component for the authorization form

---
+++ src/components/AuthorizationComponent.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container-reg">
+    <button class="btn-close">
+      <img src="../assets/images/close.svg" />
+    </button>
+    <div class="container">
+      <h2>Вход в ваш аккаунт</h2>
+      <div class="input-container">
+        <div class="input-field">
+          <label for="email">
+            Email
+          </label>
+          <input
+              name="email"
+              type="email"
+              placeholder="Введите Email" />
+        </div>
+        <div class="input-field">
+          <label for="password">
+            Пароль
+          </label>
+          <input
+              name="password"
+              type="password"
+              placeholder="Введите пароль" />
+        </div>
+      </div>
+      <div class="link-container">
+        <div class="link">
+          <p>У вас нет аккаунта?</p>
+          <a href="#">Зарегистрируйтесь</a>
+        </div>
+        <button type="submit">
+          Зарегистрироваться
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang='ts' setup></script>
