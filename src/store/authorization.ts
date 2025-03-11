cb5ef3b48673851e0498d7b39c9210722c82531b
TatianaChF <tatiana.chefox@gmail.com>
2025-03-11 18:43:11 +0300

    adding methods for validation and submission of the registration form

--- src/store/authorization.ts
+++ src/store/authorization.ts
@@ -1,5 +1,6 @@
 import {defineStore} from "pinia";
 import {reactive} from "vue";
+import type ApiResponse from "./registration.ts";
 
 interface AuthorizationData {
     email: string;
@@ -18,5 +19,55 @@ export const useAuthorizationStore = defineStore('authorizationData', () => {
     });
     const errors: AuthErrors = reactive({});
 
-    return {authorizationData, errors}
+    const validateEmail = () => {
+        if (!authorizationData.email) {
+            errors.email = "Email обязателен для входа";
+        } else errors.email = undefined;
+    };
+
+    const validatePassword = () => {
+        if (!authorizationData.password) {
+            errors.password = "Пароль обязателен для входа";
+        } else errors.password = undefined;
+    };
+
+    const resetForm = () => {
+        authorizationData.email = "";
+        authorizationData.password = "";
+
+        Object.keys(errors).forEach(key => {
+            errors[key as keyof AuthErrors] = undefined;
+        });
+    }
+
+    const handleSubmit = async () => {
+        try {
+            validateEmail();
+            validatePassword();
+
+            const response = await fetch('https://dist.nd.ru/api/auth', {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    email: authorizationData.email,
+                    password: authorizationData.password,
+                })
+            });
+            const data: ApiResponse = await response.json();
+
+            if (data.id) {
+                resetForm();
+                console.log(data)
+            } else {
+                console.log(data)
+                errors.email = data.message;
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    return {authorizationData, errors, validateEmail, validatePassword, handleSubmit}
 })
