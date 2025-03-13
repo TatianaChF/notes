9030746f0d2b72a1a30c798131e0bfc63e4259a0
TatianaChF <tatiana.chefox@gmail.com>
2025-03-13 16:09:40 +0300

    add form from note

---
+++ src/components/NoteForm.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container-reg">
+    <button
+        class="btn-close">
+      <img src="../assets/images/close.svg" />
+    </button>
+    <div class="container">
+      <h2>Добавление заметки</h2>
+      <div class="input-container">
+        <div class="input-field">
+          <label for="title">
+            Название заметки
+          </label>
+          <input
+              name="title"
+              type="text"
+              placeholder="Введите название"/>
+        </div>
+        <div class="input-field">
+          <label for="text">
+            Текст заметки
+          </label>
+          <textarea
+              name="text"
+              placeholder="Введите текст" />
+        </div>
+      </div>
+      <button type="submit">
+        Добавить
+      </button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
