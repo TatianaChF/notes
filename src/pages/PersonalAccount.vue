a30939f84a0ad07b3fb6cba9c4e42179fa75d0c4
TatianaChF <tatiana.chefox@gmail.com>
2025-03-15 12:18:59 +0300

    add storeToRefs

--- src/pages/PersonalAccount.vue
+++ src/pages/PersonalAccount.vue
@@ -2,13 +2,15 @@
   {{authorizationStore.userData.email}}
   <div class="notes-container">
     <div
-        v-for="note in personalStore.notes"
-        :key="note.title"
+        v-for="note in notes"
+        :key="note.id"
         class="note">
       <h4>{{note.title}}</h4>
       <hr class="note-divider" />
       <p>{{note.content}}</p>
-      <div class="note-delete">
+      <div
+          class="note-delete"
+          @click="personalStore.removeNote(note);">
         <img src="../assets/images/close.svg" alt="delete" />
         <p>Удалить</p>
       </div>
@@ -28,16 +30,17 @@
 
 <script lang="ts" setup>
 import {onMounted, ref} from "vue";
-import {usePersonalStore} from "../store/personal.ts";
+import {type Note, usePersonalStore} from "../store/personal.ts";
 import {useAuthorizationStore} from "../store/authorization.ts";
 import NoteForm from "../components/NoteForm.vue";
+import {storeToRefs} from "pinia";
 
 const personalStore = usePersonalStore();
 const authorizationStore = useAuthorizationStore();
 const isOpenForm = ref(false);
+const { notes } = storeToRefs(personalStore);
 
 onMounted(() => {
   personalStore.getNotes();
 })
-
 </script>
